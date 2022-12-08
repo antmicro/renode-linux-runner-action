57e371d527e51be9ea0c179204fb46e520eb71d9
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-12-08 16:59:41 +0100

    renode-tester-docker: update
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- renode-tester-docker/payload/entrypoint.sh
+++ renode-tester-docker/payload/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# !!! Copyright is missing here
+
+set -e
+
+# TODO: Can this be included in run-in-renode's main so that the entrypoint is run-in-renode itself?
+
 cd /github/workspace
 truncate drive.img -s 100M
 mkfs.ext4 -d "$1" drive.img > /dev/null
