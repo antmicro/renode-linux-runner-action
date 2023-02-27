571f507635d78b70a70b9c540bc625dca568cb1a
Wiktor Ogrodnik <wogrodnik@internships.antmicro.com>
2023-02-27 12:23:13 +0100

    Adds gpio_mockup to the image
    -Adds script to start docker and tests locally
    -Adds gpio_mockup to the kernel image, disables gpio native module from renode
    -Adds mechanism for adding new devices easly in run-in-renode.py script
    -Adds `devices` as new action argument
    -Adds workflow to test pull requests

---
+++ tests/gpio.sh
@@ -0,0 +1,27 @@
+print_result()
+{
+    if [[ $1 -eq $2 ]]; then
+        echo Done!
+    else
+        echo FAIL!
+        exit 1
+    fi
+}
+
+# test gpio_mockup getter
+
+echo 1 > /sys/kernel/debug/gpio-mockup/gpiochip0/10
+print_result $(gpioget gpiochip0 10) 1
+
+echo 0 > /sys/kernel/debug/gpio-mockup/gpiochip0/10
+print_result $(gpioget gpiochip0 10) 0
+
+
+# test gpio_mockup setter
+
+gpioset --mode=time --sec=1 -b gpiochip0 10=1
+print_result $(cat /sys/kernel/debug/gpio-mockup/gpiochip0/10) 1
+
+sleep 1
+
+print_result $(cat /sys/kernel/debug/gpio-mockup/gpiochip0/10) 0
