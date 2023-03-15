fa9d262b9880390fe253f4621e281ab9f3fe6b42
Wiktor Ogrodnik <wogrodnik@internships.antmicro.com>
2023-03-15 10:35:50 +0100

    New linux image creation method (#8)
    Uses buildroot external method
    Fixes in extract_arguments.py script
    Bumps Linux kernel from 5.10.0 to 5.10.162
    Bumps Buildroot version from 2022.08.1 to 2022.11.2

--- extract_arguments.py
+++ extract_arguments.py
@@ -2,7 +2,7 @@ import yaml
 from sys import argv, exit
 
 path = ['jobs',
-        'renode-linux-runner-test',
+        'default-configuration-test',
         'steps',
         2,
         'with']
@@ -11,7 +11,7 @@ def ret_arg(arg: str) -> str:
     global path
     path += [arg]
 
-    with open("./.github/workflows/run_action.yml", "r", encoding="utf-8") as yml:
+    with open("./.github/workflows/push.yml", "r", encoding="utf-8") as yml:
         obj = yaml.safe_load(yml)
         for p in path:
             obj = obj[p]
