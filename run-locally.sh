c5b2b8808d96924b09afd79bae0f631cfdcf3c4c
Wiktor Ogrodnik <wogrodnik@internships.antmicro.com>
2023-03-23 11:23:52 +0100

    Enable Internet in Renode environment (#10)
    - Adds TAP interface to docker configuration
    - Adds network configuration for buildroot linux
    - Adds test to test network in simulated renode environment
    - Adds additional flags to docker in the action.yml file

--- run-locally.sh
+++ run-locally.sh
@@ -2,6 +2,7 @@
 
 IMAGE_PATH=renode-linux-runner-docker
 DOCKERFILE_PATH=$IMAGE_PATH/Dockerfile
+SHARED_DIR="$(python3 extract_arguments.py shared-dir)"
 
 mkdir $IMAGE_PATH/tests
 cp -r -f tests/* $IMAGE_PATH/tests/
@@ -11,7 +12,9 @@ git clone https://github.com/antmicro/pyrav4l2.git $IMAGE_PATH/tests/pyrav4l2
 docker build -t $IMAGE_PATH      \
              -f $DOCKERFILE_PATH \
               $IMAGE_PATH &&     \
-docker run -v "$(pwd)"/$IMAGE_PATH/tests:/mnt/user         \
+docker run --cap-add=NET_ADMIN                             \
+           --device /dev/net/tun:/dev/net/tun              \
+           -v "$(pwd)"/$IMAGE_PATH/$SHARED_DIR:/mnt/user   \
            $IMAGE_PATH                                     \
            "$(python3 extract_arguments.py renode-run)"    \
            "$(python3 extract_arguments.py devices)"
