8e2a17b78fe0da38ad3a65c6c5f193cc7f503441
Wiktor Ogrodnik <wogrodnik@internships.antmicro.com>
2023-05-04 19:58:45 +0200

    Separates initramfs and rootfs (#26)
    - adds two phases of compilation: first initramfs + kernel + bootloader,
     second: rootfs + tools (python, git, pip, ...)
    - speeds up Linux boot time to a few seconds
    - more flexible approach: adding new tools to rootfs will not
     significantly increase boot time

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,16 +1,39 @@
 #!/bin/sh
 
+mkdir -p images
 git clone --branch 2022.11.3 https://github.com/buildroot/buildroot buildroot
 
-cd buildroot && \
-make BR2_EXTERNAL=../br2-external hifive_unleashed_defconfig && \
+cd buildroot
+
+# Set configuration for initramfs
+
+make BR2_EXTERNAL=../br2-external hifive_unleashed_defconfig
 make -j$(nproc)
 
 cd ..
-mkdir -p images
 
 for i in hifive-unleashed-a00.dtb fw_payload.elf Image rootfs.cpio; do
-    cp buildroot/output/images/$i images;
+    cp buildroot/output/images/$i images
 done
+
+# Clear only target binaries, do not delete host toolchain that will be used again
+# More info: https://stackoverflow.com/questions/47320800/how-to-clean-only-target-in-buildroot
+
+cd buildroot
+
+rm -rf output/target
+find output/ -name ".stamp_target_installed" -delete
+rm -f output/build/host-gcc-final-*/.stamp_host_installed
+
+
+# Set configuration for rootfs
+
+make BR2_EXTERNAL=../br2-external/rootfs hifive_unleashed_defconfig
+make -j$(nproc)
+
+cd ..
+
+cp buildroot/output/images/rootfs.img images
+rm -rf buildroot
 
 tar cJvf images.tar.xz images
